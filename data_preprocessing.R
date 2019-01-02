6cb0869fd1733c4409bfff0f53f9241068b24cb1
Ryan <rkookorean@gmail.com>
2019-01-01 19:01:04 -0600

    fixed encoding issue

--- data_preprocessing.R
+++ data_preprocessing.R
@@ -13,3 +13,12 @@ dataset$Age = ifelse(is.na(dataset$Age),
 dataset$Salary = ifelse(is.na(dataset$Salary), 
                      ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)), 
                      dataset$Salary) 
+
+# Encoding categorical data
+dataset$Country = factor(dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1,2,3)) # here we are creating a vector of three elements: France, Spain, Germany
+dataset$Purchased = factor(dataset$Purchased,
+                           levels = c('No', 'Yes'),
+                           labels = c(0, 1)) 
+
